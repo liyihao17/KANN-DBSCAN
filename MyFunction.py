de2e0131595e4f9d07f803e7e535330a4e036825
Reky <623694789@qq.com>
2019-04-18 16:15:51 +0800

    更新了注释

--- MyFunction.py
+++ MyFunction.py
@@ -21,15 +21,17 @@ def loadDataSet(fileName, splitChar='\t'):
 
 def dist(a,b):
     """
+    用来计算两个样本点之间的距离
     :param a: 样本点
     :param b: 样本点
-    :return: 两个样本点之间的欧式距离
+    :return: 两个样本点之间的距离
     """
     return math.sqrt(math.pow(a[0]-b[0],2) + math.pow(a[1]-b[1],2))
 
 
 def returnDk(matrix,k):
     """
+    用来计算第K最近的距离集合
     :param matrix: 距离矩阵
     :param k: 第k最近
     :return: 第k最近距离集合
@@ -42,6 +44,7 @@ def returnDk(matrix,k):
 
 def returnDkAverage(Dk):
     """
+    求第K最近距离集合的平均值
     :param Dk: k-最近距离集合
     :return: Dk的平均值
     """
@@ -53,6 +56,7 @@ def returnDkAverage(Dk):
 
 def CalculateDistMatrix(dataset):
     """
+    计算距离矩阵
     :param dataset: 数据集
     :return: 距离矩阵
     """
@@ -65,6 +69,7 @@ def CalculateDistMatrix(dataset):
 
 def returnEpsCandidate(dataSet):
     """
+    计算Eps候选列表
     :param dataSet: 数据集
     :return: eps候选集合
     """
@@ -82,6 +87,7 @@ def returnEpsCandidate(dataSet):
 
 def returnMinptsCandidate(DistMatrix,EpsCandidate):
     """
+    计算Minpts候选列表
     :param DistMatrix: 距离矩阵
     :param EpsCandidate: Eps候选列表
     :return: Minpts候选列表
@@ -100,6 +106,7 @@ def returnMinptsCandidate(DistMatrix,EpsCandidate):
 
 def returnClusterNumberList(dataset,EpsCandidate,MinptsCandidate):
     """
+    计算聚类后的类别数目
     :param dataset: 数据集
     :param EpsCandidate: Eps候选列表
     :param MinptsCandidate: Minpts候选列表
